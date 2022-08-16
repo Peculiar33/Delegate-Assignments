bc216b612868c8485357abc7ddf054a5f7bee055
Peculiar <onugbolup2019@gmail.com>
2022-08-16 09:05:43 +0100

    Add project files.

---
+++ Delegate Assignments/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Delegate_Assignments
+{
+    internal class Program
+    {
+        public delegate string UserName();
+        public delegate bool Predicate(TClass t);
+        static void Main(string[] args)
+        {
+            //  var likelyUsername = t.FirstName + "." + t.LastName;
+            Delegate classwork = (TClass t) =>
+            {
+                if (t.UserName == "Perks")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            };
+        }
+    }
+
+    class TClass
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string UserName { get; set; }
+    }
+}
